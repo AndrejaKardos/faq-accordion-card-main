6a052853ee9d1c6883aab8c94bd402357d14e2d7
AndrejaKardos <andreja.kardos@gmail.com>
2021-02-03 13:22:45 +1100

    Added fonts toggling bold/normal when clicked on

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
+function toggleFont(num) {
+    document.getElementById('question-' + num).style.fontWeight = document.getElementById('question-' + num).style.fontWeight === '700' ? '' : '700';
+}
+
 for (let i = 1; i < 6; i++) {
     document.getElementById('question-' + i).addEventListener('click', function() {
+        toggleFont(i);
         document.getElementById('answer-' + i).classList.toggle('answer--hide');
     });
 }
