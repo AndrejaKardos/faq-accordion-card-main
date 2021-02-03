170580591eaf7ec907cd977188bcf3fab6ac51bc
AndrejaKardos <andreja.kardos@gmail.com>
2021-02-03 12:01:13 +1100

    Added hide/show toggle for first Q&A

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-let question = document.getElementsByClassName('.question');
-let answer = document.getElementsByClassName('.answer');
+// let question = document.getElementsByClassName('question');
+// let answer = document.getElementsByClassName('answer');
 
-document.getElementsByClassName('.question').addEventListener('click', function() {
-    document.getElementsByClassName('.answer').removeClass('.answer--hide');
+document.getElementById('question-1').addEventListener('click', function() {
+    console.log("Hello!")
+    document.getElementById('answer-1').classList.toggle('answer--hide');
 });
 
 // Need to make an array for each one, otherwise this applies to all answers and questions
