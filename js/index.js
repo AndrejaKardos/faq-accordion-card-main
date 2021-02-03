3c496032bbb388f517e8e03186ecac1dd82cae75
AndrejaKardos <andreja.kardos@gmail.com>
2021-02-03 12:04:18 +1100

    Added margin to answers

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
-// let question = document.getElementsByClassName('question');
-// let answer = document.getElementsByClassName('answer');
-
 document.getElementById('question-1').addEventListener('click', function() {
     console.log("Hello!")
     document.getElementById('answer-1').classList.toggle('answer--hide');
 });
-
-// Need to make an array for each one, otherwise this applies to all answers and questions
